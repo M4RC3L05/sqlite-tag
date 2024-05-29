24c664593c8b1cdb62d5693456329dcd1009ab30
m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>
2024-05-29 17:01:28 +0100

    Update deps
    Signed-off-by: m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>

---
+++ src/test-deps.ts
@@ -0,0 +1,3 @@
+export { assertEquals } from "@std/assert";
+export { describe, it } from "@std/testing/bdd";
+export { Buffer } from "node:buffer";
--- src/test_deps.ts
+++
@@ -1,3 +0,0 @@
-export { assertEquals } from "jsr:@std/assert@^0.220.1";
-export { describe, it } from "jsr:@std/testing@^0.220.1/bdd";
-export { Buffer } from "node:buffer";
